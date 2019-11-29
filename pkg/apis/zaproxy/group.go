72a0b23ec9c01401e988cbb18bdb38694adde77e
omerlh <omerl@soluto.com>
2019-11-29 12:40:44 +0200

    initial commit

---
+++ pkg/apis/zaproxy/group.go
@@ -0,0 +1,6 @@
+// Package zaproxy contains zaproxy API versions.
+//
+// This file ensures Go source parsers acknowledge the zaproxy package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package zaproxy
